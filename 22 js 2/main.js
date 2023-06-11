3fd8410dbec8ba853872e49fe945a44f3ac10255
Mushketov <rootanswer@gmail.com>
2023-06-11 13:23:13 +0300

    :fire: :

---
+++ 22 js 2/main.js
@@ -0,0 +1,14 @@
+let color = "blue776";
+let arr;
+
+// if (color === "red") {
+//   arr = true;
+// } else if (color === "green") {
+//   arr = false;
+// } else {
+//   console.log("error");
+// }
+// console.log(arr);
+
+color === "red" ? (arr = "крас") : color === "blue" ? (arr = "син") : color === "green" ? (arr = "зел") : console.log("error");
+console.log(arr);
